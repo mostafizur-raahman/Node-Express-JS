6a6d3157ed2983d5b7a4b8027c8bc8b7d8a152b9
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2024-02-10 22:47:34 +0600

    implement new user in admin route

--- requestObject.js
+++ requestObject.js
@@ -11,6 +11,11 @@ adminRoute.get("/dashboard", (req, res) => {
     console.log(req.path);
     res.status(200).send("This is admin Route.");
 });
+adminRoute.post("/dashboard/new-user", (req, res) => {
+    // console.log(req.baseUrl);
+    console.log(req.path);
+    res.status(200).send("This is admin Route.");
+});
 
 app.use("/admin", adminRoute);
 app.use(cookieParser());
@@ -26,3 +31,10 @@ app.get("/user/:id", (req, res) => {
 app.listen(PORT, () => {
     console.log(`server running on PORT ${PORT}`);
 });
+
+/*
+1.If you want to using cookies parser then use cookie-parser
+2.
+
+
+*/
